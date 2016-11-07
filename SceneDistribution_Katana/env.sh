bd68fb3c5f81a8cda4c8e679360334bf1af98760
theseim <sspielma@filmakademie.de>
2016-11-07 17:32:39 +0100

    - update license.txt - cleanup make files

--- SceneDistribution_Katana/env.sh
+++ SceneDistribution_Katana/env.sh
@@ -3,24 +3,11 @@
 ####################
 # path to where katana or katanaPlayer was installed
 export KATANA_HOME=/Foundry/Katana/Katana2.0v1b5-linux-x86-release-64
-export KATANA_PLAYER_HOME=/Foundry/KatanaPlayer/KatanaPlayer1.0FMX-linux-x86-release-64
-
-export KATANA_RESOURCES=/Dreamspace/OpenRL1.0v1a2/Resources
 
 # path to boost devel libs
 # tested with v. 1.46.0
-export BOOST_HOME=usr/lib64
-export BOOST_INCLUDE=$BOOST_HOME/../include
-
 export PATH=$PATH:$KATANA_HOME/bin
-
 export GLM=/Dreamspace/OpenRL1.0v1a2/OpenRLPlugin/src/glm
-
-
-#export SOIL=/Dreamspace/OpenRL1.0v1a2/OpenRLPlugin/src/soil
-#export GLEW=/Dreamspace/OpenRL1.0v1a2/OpenRLPlugin/src/glew-1.13.0
-
-
 
 # path to zeromq devel libs
 # NOTE: required by the api only if building display drivers
