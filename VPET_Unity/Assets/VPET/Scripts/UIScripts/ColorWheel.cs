ff0a7778e00545710f79658915fa1abdf475f385
smoi23 <smoi@gmx.net>
2016-11-22 10:15:38 +0100

    Update License

--- VPET_Unity/Assets/VPET/Scripts/UIScripts/ColorWheel.cs
+++ VPET_Unity/Assets/VPET/Scripts/UIScripts/ColorWheel.cs
@@ -11,17 +11,15 @@ under grant agreement no 610005.
 http://dreamspaceproject.eu/
 
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free Software
-Foundation; version 2.1 of the License.
+the terms of the MIT License as published by the Open Source Initiative.
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+FOR A PARTICULAR PURPOSE. See the MIT License for more details.
 
-You should have received a copy of the GNU Lesser General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+You should have received a copy of the MIT License along with
+this program; if not go to
+https://opensource.org/licenses/MIT
 -----------------------------------------------------------------------------
 */
 using UnityEngine;
--- VPET_Unity/Assets/VPET/Scripts/UIScripts/ConfigWidget.cs
+++ VPET_Unity/Assets/VPET/Scripts/UIScripts/ConfigWidget.cs
@@ -287,20 +287,30 @@ namespace vpet
             }
 #endif
 
-            /*
-	        // Dropdown Local Scenes
-	        childWidget = this.transform.FindChild("SceneList_dropdown");
-	        if (childWidget == null) Debug.LogError(string.Format("{0}: Cant Find: SceneList_dropdown.", this.GetType()));
-	        else
-	        {
-	            Dropdown widgetField = childWidget.GetComponent<Dropdown>();
-	            if (widgetField == null) Debug.LogError(string.Format("{0}: Cant Component: Dropdown.", this.GetType()));
-	            else
-	            {
-	                // sceneFileName = widgetField.itemText.text;
-	            }
-	        }	
-            */
+/*
+-----------------------------------------------------------------------------
+This source file is part of VPET - Virtual Production Editing Tool
+http://vpet.research.animationsinstitut.de/
+http://github.com/FilmakademieRnd/VPET
+
+Copyright (c) 2016 Filmakademie Baden-Wuerttemberg, Institute of Animation
+
+This project has been realized in the scope of the EU funded project Dreamspace
+under grant agreement no 610005.
+http://dreamspaceproject.eu/
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the MIT License as published by the Open Source Initiative.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+
+You should have received a copy of the MIT License along with
+this program; if not go to
+https://opensource.org/licenses/MIT
+-----------------------------------------------------------------------------
+*/
         }
 
 
