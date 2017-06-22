06285b83488f305c3fe9852bd80c39be64f33ba6
theseim <sspielma@filmakademie.de>
2017-06-22 22:47:02 +0200

    - disable 2nd data stream for recording in sync server - add function for requesting actual scene state from sync server

--- VPET_Unity/Assets/VPET/Scripts/IO/ServerAdapter.cs
+++ VPET_Unity/Assets/VPET/Scripts/IO/ServerAdapter.cs
@@ -316,7 +316,8 @@ namespace vpet
                 m_sceneTransferDirty = false;
                 print( "sceneLoader.createSceneGraph" );
 	            sceneLoader.createSceneGraph( );
-	                    
+
+                sendUpdateObjects();
 	            // HACK
 	            mainController.repositionCamera();
 	            // Camera.main.GetComponent<MoveCamera>().calibrate();
@@ -357,25 +358,25 @@ namespace vpet
 	    //! @param  newPosition     new absolute position of GameObject in world space
 	    public void sendTransform( Transform obj, bool onlyToClientsWithoutPhysics = false )
 	    {
-	        Vector3 pos = obj.transform.localPosition;
-	        Vector3 rot = obj.localRotation.eulerAngles;
-	        Vector3 scl = obj.transform.localScale;
+	        //Vector3 pos = obj.transform.localPosition;
+	        //Vector3 rot = obj.localRotation.eulerAngles;
+	        //Vector3 scl = obj.transform.localScale;
 	
-	        if ( !deactivatePublish )
-	        {
+	        //if ( !deactivatePublish )
+	        //{
 	
-	            string sendMessage = id + "|" + this.getPathString( obj, scene ) +
-	                                "|" + pos.x + "|" + pos.y + "|" + pos.z + 
-	                                "|" + rot.x + "|" + rot.y + "|" + rot.z +
-	                                "|" + scl.x + "|" + scl.y + "|" + scl.z;
+	        //    string sendMessage = id + "|" + this.getPathString( obj, scene ) +
+	        //                        "|" + pos.x + "|" + pos.y + "|" + pos.z + 
+	        //                        "|" + rot.x + "|" + rot.y + "|" + rot.z +
+	        //                        "|" + scl.x + "|" + scl.y + "|" + scl.z;
 	
-	            if (onlyToClientsWithoutPhysics)
-	            {
-	                sendMessage += "|physics";
-	            }
+	        //    if (onlyToClientsWithoutPhysics)
+	        //    {
+	        //        sendMessage += "|physics";
+	        //    }
 	
-	            sendMessageQueue.Add( sendMessage );
-	        }
+	        //    sendMessageQueue.Add( sendMessage );
+	        //}
 	    }
 	
 	
@@ -660,10 +661,16 @@ namespace vpet
                 currentlyLockedObject = null;
 	        }
 	    }
-	
-	    //! function parsing received message and executing change
-	    //! @param  message         message string received by server
-	    public void parseTransformation(string message)
+
+        //! function to be called to resend stored scene object attributes
+        public void sendUpdateObjects()
+        {
+            sendMessageQueue.Add(id + "|" + "udOb");
+        }
+
+        //! function parsing received message and executing change
+        //! @param  message         message string received by server
+        public void parseTransformation(string message)
 	    {
 	        string[] splitMessage = message.Split('|');
             if (splitMessage.Length > 1)
@@ -887,7 +894,7 @@ namespace vpet
 	        {
 	            if ( sendMessageQueue.Count > 0 )
 	            {
-	                //  Debug.Log("Publisher: " + sendMessageQueue[0] as string);
+	                Debug.Log("Publisher: " + sendMessageQueue[0] as string);
 	                sender.Send("client " + sendMessageQueue[0] as string);
 	                sendMessageQueue.RemoveAt(0);
 	            }
