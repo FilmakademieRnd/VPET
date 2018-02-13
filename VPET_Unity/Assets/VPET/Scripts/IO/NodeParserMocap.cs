81cc06577ef1b1b65522e32ee034517e01c2cebd
smoi23 <smoi@gmx.net>
2018-02-13 23:40:51 +0100

    Add nodeParser and Register class

---
+++ VPET_Unity/Assets/VPET/Scripts/IO/NodeParserMocap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace vpet
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 4, CharSet = CharSet.Auto)]
+    public class SceneNodeMocap : SceneNode
+    {
+    };
+
+	public class NodeParserMocap
+	{
+		public static SceneNode ParseNode(NodeType nodeType, ref byte[] nodesByteData, ref int dataIdx)
+		{
+			if ( nodeType == NodeType.MOCAP)
+			{
+                        SceneNodeMocap sceneNodeMocap = SceneDataHandler.ByteArrayToStructure<SceneNodeMocap>(nodesByteData, ref dataIdx);
+                        return sceneNodeMocap;
+			}
+			return null;
+		}
+	}
+	
+}
---
+++ VPET_Unity/Assets/VPET/Scripts/VPETRegister.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace vpet
+{
+	// Basic 
+	// public enum NodeType { GROUP, GEO, LIGHT, CAMERA}
+
+	// Basic, Mocap
+	public enum NodeType { GROUP, GEO, LIGHT, CAMERA, MOCAP }
+
+
+	public class VPETRegister  
+	{
+		public static void RegisterNodeParser()
+		{
+			SceneDataHandler.RegisterDelegate(NodeParserBasic.ParseNode);
+		}
+	}
+}
