004afe5b714a7fd291e5646bad059c00b6da00ea
Simon Spielmann <theseim@gmx.de>
2021-03-15 16:45:31 +0100

    - add first parameter implementation

---
+++ VPET_Unity2/Assets/VPET/Core/Parameter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace vpet
+{
+    public  class Parameter<T>
+    {
+        private string name;
+        private T value;
+        public event EventHandler hasChanged;
+
+        public class changeEventArgs : EventArgs
+        {
+            public T value;
+        }
+
+        public void connectToChangeValue(EventHandler<changeEventArgs> ev)
+        {
+            ev += changeValue;
+        }
+
+        public void removeFromChangeValue(EventHandler<changeEventArgs> ev)
+        {
+            ev -= changeValue;
+        }
+
+        private void changeValue(object sender, changeEventArgs a)
+        {
+            value = a.value;
+            if (hasChanged != null)
+                hasChanged(this, null);
+        }
+    }
+
+}
