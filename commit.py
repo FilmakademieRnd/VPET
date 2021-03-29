04018393619205687e5dd4bbd5978a5dc225bb53
Simon <theseim@gmx.de>
2021-03-29 15:29:55 +0200

    - update recreated commit hook python script

--- commit.py
+++ commit.py
@@ -2,14 +2,27 @@ import subprocess
 import datetime
 import os
 
+# ' ' = unmodified
+# M = modified
+# A = added
+# D = deleted
+# R = renamed
+# C = copied
+# U = updated but unmerged
+# use only files with this git flags
 gitFlags = ['A', 'M']
+
+# use only files with this extension
 fileExtensions = ['cpp', 'h', 'hpp', 'cs']
+
+# filepath to license text
 headerFile = open('LICENSE.TXT', 'r').read()
 
-
+# function for executing shell commands and capure theyr output
 def run(command):
 	return subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True).stdout
-	
+
+# function creating new header: license + infos	
 def createHeader(filepath):
 	filename = filepath.split('/')[-1]
 	return headerFile + '\n' \
@@ -18,7 +31,8 @@ def createHeader(filepath):
 	+ '//! @past authors ' + authors + '\n' \
 	+ '//! @version ' + version + '\n' \
 	+ '//! @date ' + date + '\n'
-	
+
+# function for removing old header from file	
 def stripHeader(filepath):
 	with open(filepath, 'r') as hfile:
 		lines = hfile.readlines()
@@ -29,16 +43,21 @@ def stripHeader(filepath):
 		lines = lines[index+1:]
 		print(index)
 		return ''.join(lines)
-	
+
+# function writing new header to file
 def addHeader(filepath):
 	with open(filepath, 'r+') as file:
 		file.write(createHeader(filepath) + stripHeader(filepath))
-		
+
+# the current date and time		
 date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# gather local author name
 author = run('git config user.name').strip()
 authors = ' '
+# gather local author email
 email = run('git config user.email').strip()
-version = '0'
+# gather the file version info
+version = run('git rev-list --count HEAD').strip()
 
 process = run('git status -s')
 output = process
@@ -51,6 +70,7 @@ for line in gitLines:
 			fileExtension = os.path.splitext(filePath)[1][1:].strip().lower()
 			if fileExtension in fileExtensions:
 				if 'M' in gitParams[0]:
+					# gather all authors if file has been modified
 					authorString = (run('git log --format="%an <%ae>" ' + filePath + ' || sort -u'));
 					authors = ' '.join(authorString.split('\n'))
 				print (filePath)
