f1d6e0d96417bed087de81e63903e9af707d24dd
Simon <theseim@gmx.de>
2021-06-25 11:23:01 +0200

    - update documentation

--- VPET_Unity2/Assets/VPET/Core/Parameters/Parameter.cs
+++ VPET_Unity2/Assets/VPET/Core/Parameters/Parameter.cs
@@ -1,4 +1,27 @@
-﻿//! @file "parameter.cs"
+﻿/*
+-------------------------------------------------------------------------------
+VPET - Virtual Production Editing Tools
+vpet.research.animationsinstitut.de
+https://github.com/FilmakademieRnd/VPET
+ 
+Copyright (c) 2021 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
+ 
+This project has been initiated in the scope of the EU funded project 
+Dreamspace (http://dreamspaceproject.eu/) under grant agreement no 610005 2014-2016.
+ 
+Post Dreamspace the project has been further developed on behalf of the 
+research and development activities of Animationsinstitut.
+ 
+In 2018 some features (Character Animation Interface and USD support) were
+addressed in the scope of the EU funded project  SAUCE (https://www.sauceproject.eu/) 
+under grant agreement no 780470, 2018-2021
+ 
+VPET consists of 3 core components: VPET Unity Client, Scene Distribution and
+Syncronisation Server. They are licensed under the following terms:
+-------------------------------------------------------------------------------
+*/
+
+//! @file "parameter.cs"
 //! @brief Implementation of the vpet parameter
 //! @author Simon Spielmann
 //! @author Jonas Trottnow
