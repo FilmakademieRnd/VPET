36c621088f83c76b56b65447f26bdad5186c88ba
Jonas Trottnow <jntrottnow@gmail.com>
2022-04-05 09:48:10 +0200

    Fix: Some Module Config Menus not created in GUI

--- VPET_Unity2/Assets/VPET/Modules/UIManagerModules/MenuSelectorCreatorModule/Scripts/MenuSelectorCreatorModule.cs
+++ VPET_Unity2/Assets/VPET/Modules/UIManagerModules/MenuSelectorCreatorModule/Scripts/MenuSelectorCreatorModule.cs
@@ -51,7 +51,7 @@ namespace vpet
         //! @param sender A reference to the VPET core.
         //! @param e The event arguments for the start event.
         //!
-        protected override void Init(object sender, EventArgs e)
+        protected override void Start(object sender, EventArgs e)
         {
             // [REVIEW]
             // pls remove, only for testing...
