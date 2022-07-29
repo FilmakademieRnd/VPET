746980940bc039747c7c978b1ba64f2ce9ce7f2c
Paulo Scatena <phscatena@gmail.com>
2022-07-29 10:30:52 +0200

    Unreal helper plugin updated

--- SceneDistribution_Unreal/Plugins/VPETWindow/Source/VPETWindow/Private/VPETWindowStyle.cpp
+++ SceneDistribution_Unreal/Plugins/VPETWindow/Source/VPETWindow/Private/VPETWindowStyle.cpp
@@ -1,39 +1,9 @@
-/*
------------------------------------------------------------------------------
-This source file is part of VPET - Virtual Production Editing Tools
-http://vpet.research.animationsinstitut.de/
-http://github.com/FilmakademieRnd/VPET
-
-Copyright (c) 2020 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
-
-This project has been initiated in the scope of the EU funded project
-Dreamspace under grant agreement no 610005 in the years 2014, 2015 and 2016.
-http://dreamspaceproject.eu/
-Post Dreamspace the project has been further developed on behalf of the
-research and development activities of Animationsinstitut.
-
-The VPET component Unreal Scene Distribution is intended for research and development
-purposes only. Commercial use of any kind is not permitted.
-
-There is no support by Filmakademie. Since the Unreal Scene Distribution is available
-for free, Filmakademie shall only be liable for intent and gross negligence;
-warranty is limited to malice. Scene DistributiorUSD may under no circumstances
-be used for racist, sexual or any illegal purposes. In all non-commercial
-productions, scientific publications, prototypical non-commercial software tools,
-etc. using the Unreal Scene Distribution Filmakademie has to be named as follows:
-“VPET-Virtual Production Editing Tool by Filmakademie Baden-Württemberg,
-Animationsinstitut (http://research.animationsinstitut.de)“.
-
-In case a company or individual would like to use the Unreal Scene Distribution in
-a commercial surrounding or for commercial purposes, software based on these
-components or any part thereof, the company/individual will have to contact
-Filmakademie (research<at>filmakademie.de).
------------------------------------------------------------------------------
-*/
+// Copyright (c) 2022 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
 
 #include "VPETWindowStyle.h"
-#include "Styling/SlateStyleRegistry.h"
+#include "VPETWindow.h"
 #include "Framework/Application/SlateApplication.h"
+#include "Styling/SlateStyleRegistry.h"
 #include "Slate/SlateGameResources.h"
 #include "Interfaces/IPluginManager.h"
 
@@ -76,7 +46,7 @@ TSharedRef< FSlateStyleSet > FVPETWindowStyle::Create()
 	TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("VPETWindowStyle"));
 	Style->SetContentRoot(IPluginManager::Get().FindPlugin("VPETWindow")->GetBaseDir() / TEXT("Resources"));
 
-	Style->Set("VPETWindow.OpenPluginWindow", new IMAGE_BRUSH(TEXT("ButtonIcon_40x"), Icon40x40));
+	Style->Set("VPETWindow.PluginAction", new IMAGE_BRUSH(TEXT("ButtonIcon_40x"), Icon40x40));
 
 	return Style;
 }
