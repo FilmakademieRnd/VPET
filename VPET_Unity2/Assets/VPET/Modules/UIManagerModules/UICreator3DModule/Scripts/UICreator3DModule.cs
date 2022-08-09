81e50ff128b98d7f7056580a42c3204867e4f2f0
Simon <theseim@gmx.de>
2022-08-09 14:43:15 +0200

    - optimize modifier assets

--- VPET_Unity2/Assets/VPET/Modules/UIManagerModules/UICreator3DModule/Scripts/UICreator3DModule.cs
+++ VPET_Unity2/Assets/VPET/Modules/UIManagerModules/UICreator3DModule/Scripts/UICreator3DModule.cs
@@ -1073,7 +1073,7 @@ namespace vpet
             if (!selObj)
                 return Vector3.one;
          
-            return Vector3.one * uiScale
+            return Vector3.one * uiScale * 2.54f
                        * (Vector3.Distance(mainCamera.transform.position, selObj.transform.position)
                        * (4.0f * Mathf.Tan(0.5f * (Mathf.Deg2Rad * mainCamera.fieldOfView)))
                        * Screen.dpi / (Screen.width + Screen.height));
