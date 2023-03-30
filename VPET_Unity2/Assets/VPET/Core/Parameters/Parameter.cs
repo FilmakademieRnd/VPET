1f868094309924cdacf324e8318a33430e593281
Simon <theseim@gmx.de>
2023-03-30 16:22:32 +0200

    - add RPCParameter

--- VPET_Unity2/Assets/VPET/Core/Parameters/Parameter.cs
+++ VPET_Unity2/Assets/VPET/Core/Parameters/Parameter.cs
@@ -709,6 +709,35 @@ namespace vpet
         }
     }
 
+    public class RPCParameter<T> : Parameter<T>
+    {
+        public RPCParameter(T parameterValue, string name, ParameterObject parent = null, bool distribute = true) : base(parameterValue, name, parent, distribute)
+        {
+
+        }
+        //!
+        //! Action that will be executed when the parameter is evaluated.
+        //!
+        private Action<T> m_action;
+        public Action<T> action
+        {
+            get => m_action;
+            set => m_action = value;
+        }
+
+        //!
+        //! Function for deserializing parameter _data.
+        //! 
+        //! @param _data The byte _data to be deserialized and copyed to the parameters value.
+        //! 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override void deSerialize(byte[] data, int offset)
+        {
+            base.deSerialize(data, offset);
+            m_action.Invoke(_value);
+        }
+    }
+
     [Serializable]
     //!
     //! ListParameter class defining the fundamental functionality and interface
