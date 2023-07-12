a79104b6f9e1532d7d0559e958a92d0a81bcd12a
Jonas Trottnow <git@jt-stage.de>
2023-07-12 17:15:41 +0200

    License texts updated

--- VPET_Unity2/Assets/VPET/Core/Settings/Settings.cs
+++ VPET_Unity2/Assets/VPET/Core/Settings/Settings.cs
@@ -1,3 +1,32 @@
+/*
+VPET - Virtual Production Editing Tools
+vpet.research.animationsinstitut.de
+https://github.com/FilmakademieRnd/VPET
+
+Copyright (c) 2023 Filmakademie Baden-Wuerttemberg, Animationsinstitut R&D Lab
+
+This project has been initiated in the scope of the EU funded project
+Dreamspace (http://dreamspaceproject.eu/) under grant agreement no 610005 2014-2016.
+
+Post Dreamspace the project has been further developed on behalf of the
+research and development activities of Animationsinstitut.
+
+In 2018 some features (Character Animation Interface and USD support) were
+addressed in the scope of the EU funded project SAUCE (https://www.sauceproject.eu/)
+under grant agreement no 780470, 2018-2022
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the MIT License as published by the Open Source Initiative.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+
+You should have received a copy of the MIT License along with
+this program; if not go to
+https://opensource.org/licenses/MIT
+*/
+
 using System;
 using System.IO;
 using UnityEngine;
