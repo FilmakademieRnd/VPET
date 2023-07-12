4b1c96925cb0636d081949ae8d7e7debad151797
Simon <theseim@gmx.de>
2023-07-12 17:04:42 +0200

    - update license text in source files

--- VPET_Unity2/Assets/VPET/Core/Managers/AnimationManager/Key.cs
+++ VPET_Unity2/Assets/VPET/Core/Managers/AnimationManager/Key.cs
@@ -15,11 +15,20 @@ research and development activities of Animationsinstitut.
 In 2018 some features (Character Animation Interface and USD support) were
 addressed in the scope of the EU funded project  SAUCE (https://www.sauceproject.eu/) 
 under grant agreement no 780470, 2018-2022
- 
-VPET consists of 3 core components: VPET Unity Client, Scene Distribution and
-Syncronisation Server. They are licensed under the following terms:
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the MIT License as published by the Open Source Initiative.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the MIT License for more details.
+
+You should have received a copy of the MIT License along with
+this program; if not go to
+https://opensource.org/licenses/MIT
 -------------------------------------------------------------------------------
 */
+
 
 //! @file "key.cs"
 //! @brief Implementation of the vpet parameter key
