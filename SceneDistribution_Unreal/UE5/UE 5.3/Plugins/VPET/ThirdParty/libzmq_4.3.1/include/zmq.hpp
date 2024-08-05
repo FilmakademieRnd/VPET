33ea2c87e0066ca6ae765daa385aa678cd53e68d
Simon Spielmann <sspielma@filmakademie.de>
2024-08-05 16:59:46 +0200

    - remove sceneDistribution sources - they are from now on available here: https://github.com/FilmakademieRnd/TracerSceneDistribution

--- SceneDistribution_Unreal/UE5/UE 5.3/Plugins/VPET/ThirdParty/libzmq_4.3.1/include/zmq.hpp
+++
@@ -1,9 +0,0 @@
-
-#if PLATFORM_WINDOWS && !defined(WINDOWS_PLATFORM_TYPES_GUARD)
-	#include <Windows/AllowWindowsPlatformTypes.h>
-	#include "zmq_impl.hpp"
-	#include <Windows/HideWindowsPlatformTypes.h>
-#else
-	#include "zmq_impl.hpp"
-#endif
-
--- SceneDistribution_Unreal/UE5/UE 5.3/README.txt
+++
@@ -1,3 +0,0 @@
-copy the ARM folder into 
-
-Epic Games\UE_5.2\Engine\Binaries\ThirdParty
